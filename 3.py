5f165a9bb5721659b08fac95ca4611fb4b69a1df
Елисей <monster_2003@list.ru>
2020-06-10 20:04:27 +0500

    debug

--- 3.py
+++ 3.py
@@ -1,17 +1,16 @@
 nums=[-2,1,-3,4,-1,2,1,-5,4]
-maxi=-1
+maxi=nums[0]
+num=0
 tek=0
-for tek1 in nums:
-    if(maxi<0 and tek1>0): 
-        maxi=tek1;
-        tek=tek1;
-    else:
+for tek1 in nums:    
+    if(num>0):
         tek=tek+tek1
         if(tek>maxi): 
             maxi=tek            
         if(tek1>maxi):
             maxi=tek1
-            tek=tek1            
+            tek=tek1   
+    num=1         
 print(maxi)
 num2 = [[1,3],[2,6],[8,10],[15,18]]
 num2.sort(key=lambda x: (x[0], x[1]))
