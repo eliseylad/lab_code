f73badb6974c21ac86b72ce437e3b47d9601554d
Елисей <monster_2003@list.ru>
2020-06-10 18:03:04 +0500

    done

--- 3.py
+++ 3.py
@@ -13,3 +13,15 @@ for tek1 in nums:
             maxi=tek1
             tek=tek1            
 print(maxi)
+num2 = [[1,3],[2,6],[8,10],[15,18]]
+num2.sort(key=lambda x: (x[0], x[1]))
+itog=list()
+prom=num2[0]
+for tek in num2:
+    if ( prom[0]<tek[0] and prom[1]>tek[0] and tek[1]>prom[1] ):
+        prom[1]=tek[1]        
+    else:
+        itog.append(tek)
+        prom=tek    
+for tek in itog:
+    print(tek[0],tek[1])
