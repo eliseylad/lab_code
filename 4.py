af6fefe81ee6f80060d6da46e03be166933ed027
Елисей <monster_2003@list.ru>
2020-06-14 15:48:21 +0500

    done

--- 4.py
+++ 4.py
@@ -3,6 +3,29 @@ def sr(*args):
 def prnt(**kwargs):
     print(kwargs)
 
+def sdvig(nums,k):
+    if(k>0):
+        d=nums[len(nums)-1]
+        i=len(nums)-1
+        while(i>0):
+            nums[i]=nums[i-1]
+            i=i-1
+        nums[0]=d
+        sdvig(nums,k-1)
+    else:
+        print(nums)
+
+def nul(nums,k,count):
+    while(k<len(nums)-1-count):
+        if(nums[k]==0):            
+            i=k
+            while(i<len(nums)-1):
+                nums[i]=nums[i+1]
+                i=i+1
+            nums[len(nums)-1]=0           
+            nul(nums,k,count+1)
+            break
+        k=k+1
 nums=[1,2,3,4,5]
 d=dict(name="elisey",age=20)
 sr(*nums)
@@ -22,3 +45,14 @@ except Exception as e:
 else:
     print("wow")
 print("finally будет выполнено в любом случае, а else только если ошибки не было")
+
+sdvig(nums,-3)
+#тесты:
+#nums=[1,2,3,4,5,6,7] sdvig(nums,1) sdvig(nums,7) sdvig(nums,0) sdvig(nums,-1) sdvig(nums,-0) sdvig(nums,6)
+#nums=[1,2] sdvig(nums,1) sdvig(nums,0) sdvig(nums,2) sdvig(nums,3) sdvig(nums,4) sdvig(nums,8) sdvig(nums,-2)
+#nums=[1] sdvig(nums,0) sdvig(nums,1) sdvig(nums,2) sdvig(nums,5) sdvig(nums,-3) sdvig(nums,-5) sdvig(nums,-1)
+nums=[0,0,3,0,12,13,2,2,2]
+nul(nums,0,0)
+print(nums)
+#тесты
+#nums=
